75bebda5eeb7d42ed474117528ac8ff3bed9403d
Chris McCoy <chris@lod.com>
2015-02-02 19:09:08 -0500

    added plugin options, plus misc other features

--- plugin.php
+++ plugin.php
@@ -62,6 +62,224 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
 			add_action( 'wp_enqueue_scripts', array( $this, 'woo_remove_lightboxes'), 99 );
 			add_filter('woocommerce_single_product_image_html', array( $this, 'fresco_woocommerce_lightbox'), 99, 1); 
 			add_filter('woocommerce_single_product_image_thumbnail_html', array( $this, 'fresco_woocommerce_lightbox'), 99, 1); 
+			add_action( 'admin_menu', array( $this, 'fresco_add_admin_menu' ));
+			add_action( 'admin_init', array( $this, 'fresco_settings_init' ));
+		}
+
+		/**
+		 * add options page 
+		 *
+		 * @since 1.0
+		 */
+		function fresco_add_admin_menu() { 
+			add_options_page( 'Fresco Lightbox', 'Fresco Lightbox', 'manage_options', 'fresco_lightbox', array($this, 'fresco_lightbox_options_page' ));
+		}
+
+		/**
+		 * adding settings section and fields
+		 *
+		 * @since 1.0
+		 */
+		function fresco_settings_init() { 
+
+			register_setting( 'fresco_plugin_page', 'fresco_settings' );
+
+			if( false == get_option( 'fresco_settings' ) ) { 
+
+				$defaults = array(
+					'fresco_ui_single' => 'outside',
+					'fresco_ui_group' => 'outside',
+					'fresco_ui_video' => 'outside',
+					'fresco_ui_woocommerce' => 'outside',
+					'fresco_group_thumbnails' => 'horizontal',
+					'fresco_thumbnails_woocommerce' => 'horizontal',
+				);
+
+				add_option( 'fresco_settings', $defaults );
+			}
+
+			add_settings_section(
+				'fresco_plugin_page_section', 
+				null,
+				null,
+				'fresco_plugin_page'
+			);
+
+			add_settings_field( 
+				'fresco_ui_single', 
+				__( 'Single Image Close Button', 'fresco' ), 
+				array($this, 'fresco_ui_single_render'), 
+				'fresco_plugin_page', 
+				'fresco_plugin_page_section' 
+			);
+
+			add_settings_field( 
+				'fresco_ui_group', 
+				__( 'Gallery Image Close Button', 'fresco' ), 
+				array($this, 'fresco_ui_group_render'), 
+				'fresco_plugin_page', 
+				'fresco_plugin_page_section' 
+			);
+
+			add_settings_field( 
+				'fresco_ui_video', 
+				__( 'Video Close Button', 'fresco' ), 
+				array($this, 'fresco_ui_video_render'), 
+				'fresco_plugin_page', 
+				'fresco_plugin_page_section' 
+			);
+
+			add_settings_field( 
+				'fresco_ui_woocommerce', 
+				__( 'WooCommerce Close Button', 'fresco' ), 
+				array($this, 'fresco_ui_woocommerce_render'), 
+				'fresco_plugin_page', 
+				'fresco_plugin_page_section' 
+			);
+
+			add_settings_field( 
+				'fresco_group_thumbnails', 
+				__( 'Thumbnail Gallery Position', 'fresco' ), 
+				array($this, 'fresco_group_thumbnails_render'), 
+				'fresco_plugin_page', 
+				'fresco_plugin_page_section' 
+			);
+
+			add_settings_field( 
+				'fresco_thumbnails_woocommerce', 
+				__( 'WooCommerce Thumbnail Gallery Position', 'fresco' ), 
+				array($this, 'fresco_thumbnails_woocommerce_render'), 
+				'fresco_plugin_page', 
+				'fresco_plugin_page_section' 
+			);
+
+
+		}
+
+		/**
+		 * render select field for fresco UI Single Images
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_ui_single_render() { 
+
+			$options = get_option( 'fresco_settings' );
+			?>
+			<select name='fresco_settings[fresco_ui_single]'>
+				<option value='outside' <?php selected( $options['fresco_ui_single'], 'outside' ); ?>>Outside (default)</option>
+				<option value='inside' <?php selected( $options['fresco_ui_single'], 'inside' ); ?>>Inside</option>
+			</select>
+		<?php
+		}
+
+		/**
+		 * render select field for fresco UI group settings
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_ui_group_render() { 
+
+			$options = get_option( 'fresco_settings' );
+			?>
+			<select name='fresco_settings[fresco_ui_group]'>
+				<option value='outside' <?php selected( $options['fresco_ui_group'], 'outside' ); ?>>Outside (default)</option>
+				<option value='inside' <?php selected( $options['fresco_ui_group'], 'inside' ); ?>>Inside</option>
+			</select>
+		<?php
+		}
+
+		/**
+		 * render select field for fresco UI video settings
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_ui_video_render() { 
+
+			$options = get_option( 'fresco_settings' );
+			?>
+			<select name='fresco_settings[fresco_ui_video]'>
+				<option value='outside' <?php selected( $options['fresco_ui_video'], 'outside' ); ?>>Outside (default)</option>
+				<option value='inside' <?php selected( $options['fresco_ui_video'], 'inside' ); ?>>Inside</option>
+			</select>
+		<?php
+		}
+
+		/**
+		 * render select field for fresco UI woocommerce settings
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_ui_woocommerce_render() { 
+
+			$options = get_option( 'fresco_settings' );
+			?>
+			<select name='fresco_settings[fresco_ui_woocommerce]'>
+				<option value='outside' <?php selected( $options['fresco_ui_woocommerce'], 'outside' ); ?>>Outside (default)</option>
+				<option value='inside' <?php selected( $options['fresco_ui_woocommerce'], 'inside' ); ?>>Inside</option>
+			</select>
+		<?php
+		}
+
+		/**
+		 * render select field for fresco UI group settings
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_group_thumbnails_render() { 
+
+			$options = get_option( 'fresco_settings' );
+			?>
+			<select name='fresco_settings[fresco_group_thumbnails]'>
+				<option value='horizontal' <?php selected( $options['fresco_group_thumbnails'], 'horizontal' ); ?>>Horizontal (default)</option>
+				<option value='vertical' <?php selected( $options['fresco_group_thumbnails'], 'vertical' ); ?>>Vertical</option>
+				<option value='false' <?php selected( $options['fresco_group_thumbnails'], 'false' ); ?>>Off</option>
+			</select>
+		<?php
+		}
+
+		/**
+		 * render select field for fresco UI group settings
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_thumbnails_woocommerce_render() { 
+
+			$options = get_option( 'fresco_settings' );
+			?>
+			<select name='fresco_settings[fresco_thumbnails_woocommerce]'>
+				<option value='horizontal' <?php selected( $options['fresco_thumbnails_woocommerce'], 'horizontal' ); ?>>Horizontal (default)</option>
+				<option value='vertical' <?php selected( $options['fresco_thumbnails_woocommerce'], 'vertical' ); ?>>Vertical</option>
+				<option value='false' <?php selected( $options['fresco_thumbnails_woocommerce'], 'false' ); ?>>Off</option>
+			</select>
+		<?php
+		}
+
+		/**
+		 * settings options page form
+		 *
+		 * @since 1.0
+		 */
+
+		function fresco_lightbox_options_page() { 
+
+			?>
+			<form action='options.php' method='post'>
+
+				<h2>Fresco Lightbox</h2>
+				<b>Options for the Fresco Lightbox WordPress Plugin</b>
+				<?php
+				settings_fields( 'fresco_plugin_page' );
+				do_settings_sections( 'fresco_plugin_page' );
+				submit_button();
+				?>
+			</form>
+			<?php
 		}
 
 		/**
@@ -126,12 +344,15 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
 
 		function media_filter($html, $attachment_id) {
 
+			$position_option = get_option( 'fresco_settings' );
+			$position = "'" . $position_option['fresco_ui_single'] . "'";
+
     			$attachment = get_post($attachment_id);
 
 			$types = array('image/jpeg', 'image/gif', 'image/png');
 
 			if(in_array($attachment->post_mime_type, $types) ) {
-				$fresco_attr = sprintf('class="fresco thumbnail" data-fresco-group="gallery-%s"', $attachment->post_parent);
+				$fresco_attr = sprintf('class="fresco thumbnail" data-fresco-group="gallery-%s" data-fresco-options="ui: %s"', $attachment->post_parent, $position);
     				$html = '<a href="'. wp_get_attachment_url($attachment_id) .'" '. $fresco_attr .'><img src="'. wp_get_attachment_thumb_url($attachment_id) .'"></a>';
 			}
 
@@ -157,10 +378,14 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
 
 		function embed_html( $html, $url, $args, $post_ID ) {
 
+			$position_option = get_option( 'fresco_settings' );
+			$position = "'" . $position_option['fresco_ui_video'] . "'";
+
 			$screenshot = wp_get_attachment_url( get_post_thumbnail_id($post_ID) ) ? wp_get_attachment_url( get_post_thumbnail_id($post_ID) ) : 'http://fakeimg.pl/439x230/282828/eae0d0/?text=Click%20to%20Play!';
 
                         if ( strstr($url, 'youtube.com') || strstr($url, 'vimeo.com')) {
-      		        	$html = sprintf('<a href="%1$s" class="fresco"><img src="%2$s" /></a>', $url, $screenshot);
+				$fresco_attr = sprintf('class="fresco" data-fresco-options="ui: %s"', $position);
+      		        	$html = sprintf('<a href="%1$s" '. $fresco_attr .'><img src="%2$s" /></a>', $url, $screenshot);
         	        }
 
                      	return $html;
@@ -176,8 +401,11 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
 		function wp_embed_register_handler( $matches, $attr, $url, $rawattr ) {
 			global $post;
 
+			$position_option = get_option( 'fresco_settings' );
+			$position = "'" . $position_option['fresco_ui_single'] . "'";
+
     			if (preg_match('#^http://.+\.(jpe?g|gif|png)$#i', $url)) {
-       	       			$embed = sprintf('<a href="%s" class="fresco thumbnail" data-fresco-group="gallery-%s"><img src="%s"></a>', $matches[0], $post->ID, $matches[0]);
+       	       			$embed = sprintf('<a href="%s" class="fresco thumbnail" data-fresco-group="gallery-%s" data-fresco-options="ui: %s"><img src="%s"></a>', $matches[0], $post->ID, $position, $matches[0]);
     			}
 
 			$embed = apply_filters( 'oembed_detect_lightbox', $embed, $matches, $attr, $url, $rawattr );
@@ -192,6 +420,13 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
          	*/
 		function fresco_woocommerce_lightbox($html) {
 
+			$position_option = get_option( 'fresco_settings' );
+			$position = "'" . $position_option['fresco_ui_woocommerce'] . "'";
+			$thumbnails = "'" . $position_option['fresco_thumbnails_woocommerce'] . "'";
+			$thumbnail_show = ($thumbnails == "'false'") ? 'false' : $thumbnails;
+
+			$fresco_attr = sprintf('class="fresco" data-fresco-group-options="ui: %s, thumbnails: %s"', $position, $thumbnail_show);
+
    			$search = array(
 				'class="woocommerce-main-image zoom"',
 				'data-rel="prettyPhoto[product-gallery]"',
@@ -200,10 +435,10 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
   			);
 
    			$replace = array(
-				'class="fresco"',
+				$fresco_attr,
 				'data-fresco-group="[product-gallery]"',
 				'',
-				'class="fresco"'
+				$fresco_attr
 			);
 
    			$html = str_replace($search, $replace, $html);
@@ -303,11 +538,16 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
         				return $output;
     				}
 
+				$position_option = get_option( 'fresco_settings' );
+				$position = "'" . $position_option['fresco_ui_group'] . "'";
+				$thumbnails = "'" . $position_option['fresco_group_thumbnails'] . "'";
+				$thumbnail_show = ($thumbnails == "'false'") ? 'false' : $thumbnails;
+
     				$output = "\n" . '<div class="fresco_gallery">' . "\n";
 
     				foreach ( $attachments as $id => $attachment ) {
-					$fresco_attr = sprintf('class="fresco" data-fresco-group="gallery-%s" data-fresco-caption="%s"', $post->ID, $post->post_title);
-        				$output .= '<a href="'. wp_get_attachment_url($id) .'" '. $fresco_attr. '><img src="'. wp_get_attachment_thumb_url($id) .'" class="fresco_thumbnail"></a>' . "\n";
+					$fresco_attr = sprintf('class="fresco thumbnail" data-fresco-group="gallery-%s" data-fresco-caption="%s" data-fresco-group-options="ui: %s, thumbnails: %s"', $post->ID, $post->post_title, $position, $thumbnail_show);
+        				$output .= '<a href="'. wp_get_attachment_url($id) .'" '. $fresco_attr. '><img src="'. wp_get_attachment_thumb_url($id) .'" class="fresco thumbnail"></a>' . "\n";
     				}
 
     				$output .= "</div>" . "\n";
@@ -316,4 +556,3 @@ if( !class_exists( 'Fresco_Lightbox' ) ) {
 		}
    	}
 }
-
